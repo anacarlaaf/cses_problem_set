b680acf13a4821c6acddd3d47404bcb786ece1ee
anacarla <anacarla.daf@gmail.com>
2024-04-16 18:16:31 -0400

    atualizacoes em number_spiral

--- anotacoes.md
+++ anotacoes.md
@@ -1,2 +1,3 @@
 Number Spiral
-    Não se compara listas com !=, pois isso só verifica se essas listas são o mesmo objeto, não se possuem os mesmos itens.
+    - Não se compara listas com !=, pois isso só verifica se essas listas são o mesmo objeto, não se possuem os mesmos itens.
+    - dicionários são não ordenados
--- number_spiral.py
+++ number_spiral.py
@@ -1,75 +1,64 @@
+# todos com índice 1 tão dando 0
+# tirar index
+
 t = int(input())
 casos = []
 for i in range(t):
     y, x = list(map(int, input().split()))
     casos.append([y, x])
-# print(casos)
-
-resultado = []
 
 # verificar qual o maior quadrado formado
 maior = 0
 for i in casos:
     if i[0] > maior:
         maior = i[0]
-    elif i[1] > maior:
+    if i[1] > maior:
         maior = i[1]
+print(maior)
 
+resultado = []
 linha = 1
 coluna = 1
 lado = 1
 numero = 1
-# formar todos os valores até o maior quadrado
+
+coord_numero = {(1, 1): 1, }
+
+while lado < maior:
+    coluna += 1
+    numero += 1
+    # print(numero, [linha, coluna])
+    coord_numero[(linha, coluna)] = numero
+    while linha < lado + 1:
+        linha += 1
+        numero += 1
+        # print(numero, [linha, coluna])
+        coord_numero[(linha, coluna)] = numero
+    while coluna > 1:
+        coluna -= 1
+        numero += 1
+        # print(numero, [linha, coluna])
+        coord_numero[(linha, coluna)] = numero
+    lado += 1
+    linha += 1
+    numero += 1
+    # print(numero, [linha, coluna])
+    coord_numero[(linha, coluna)] = numero
+    while coluna < lado + 1:
+        coluna += 1
+        numero += 1
+        # print(numero, [linha, coluna])
+        coord_numero[(linha, coluna)] = numero
+    while linha > 1:
+        linha -= 1
+        numero += 1
+        # print(numero, [linha, coluna])
+        coord_numero[(linha, coluna)] = numero
+    lado += 1
+
 for i in casos:
-    if i[0] == 1 and i[1] == 1:
-        resultado.append(1)
-    else:
-        # coordenada = [0, 0]
-        condicao = False
-        while condicao is False:
-            # verificar se o quadrado formado tem valor par ou impar
-            # para definir se a espiral continua em sentido horário
-            # ou anti-horário
-            if lado % 2 != 0:  # ímpar
-                coluna += 1
-                numero += 1
-                # print(numero, [linha, coluna])
-                if [linha, coluna] in casos:
-                    resultado.append(numero)
-                while linha < lado + 1:
-                    linha += 1
-                    numero += 1
-                    # print(numero, [linha, coluna])
-                    if linha == i[0] and coluna == i[1]:
-                        resultado.append(numero)
-                        condicao = True
-                while coluna > 1:
-                    coluna -= 1
-                    numero += 1
-                    # print(numero, [linha, coluna])
-                    if linha == i[0] and coluna == i[1]:
-                        resultado.append(numero)
-                        condicao = True
-                lado += 1
-            else:
-                linha += 1
-                numero += 1
-                # print(numero, [linha, coluna])
-                if [linha, coluna] in casos:
-                    resultado.append(numero)
-                while coluna < lado + 1:
-                    coluna += 1
-                    numero += 1
-                    # print(numero, [linha, coluna])
-                    if linha == i[0] and coluna == i[1]:
-                        resultado.append(numero)
-                        condicao = True
-                while linha > 1:
-                    linha -= 1
-                    numero += 1
-                    # print(numero, [linha, coluna])
-                    if linha == i[0] and coluna == i[1]:
-                        resultado.append(numero)
-                        condicao = True
-                lado += 1
-print(resultado)
+    linha = i[0]
+    coluna = i[1]
+    # print(coord_numero[(linha, coluna)])
+for i in coord_numero:
+    print(i, " - ", coord_numero[i])
